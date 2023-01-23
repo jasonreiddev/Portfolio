2e584a94ccf4b98016c22f9cb7fc871e7d587503
Jason Reid <jasonreid@netconstruct.co.uk>
2023-01-23 23:10:13 +0000

    Button link handling

--- src/stories/components/Button/Button.tsx
+++ src/stories/components/Button/Button.tsx
@@ -1,16 +1,21 @@
 import React from "react"
+import { navigate } from "gatsby"
 import { ButtonStyles as s, ButtonStylesProps } from "./Button.styles"
 
 export interface ButtonProps extends ButtonStylesProps {
   text: string
   onClick?: () => void
-  href: string
+  href?: string
   idPrefix: string
   id: string
 }
 
+const externalLink = new RegExp("^(https?://|mailto:)", "i")
+
 export const Button = ({
   text,
+  onClick,
+  href,
   idPrefix,
   id,
   primary = false,
@@ -18,6 +23,18 @@ export const Button = ({
   // When on card w/ reversed background color
   reversed,
 }: ButtonProps) => {
+  if (!onClick && href) {
+    if (externalLink.test(href)) {
+      onClick = () => {
+        window.location.href = href
+      }
+    } else {
+      onClick = () => {
+        navigate("/page")
+      }
+    }
+  }
+
   return (
     <s.Button
       primary={primary}
@@ -25,6 +42,7 @@ export const Button = ({
       reversed={reversed}
       type="button"
       id={`${idPrefix}-${id}`}
+      onClick={onClick}
     >
       {text}
     </s.Button>
