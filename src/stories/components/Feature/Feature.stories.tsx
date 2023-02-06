393dad9b15ac2e96af2ec08e58e658dd48358ec0
Jason Reid <jasonreid@netconstruct.co.uk>
2023-02-06 17:44:11 +0000

    Fix flip and add stories

--- src/stories/components/Feature/Feature.stories.tsx
+++ src/stories/components/Feature/Feature.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentStory } from "@storybook/react"
+import { PrimarySecondary } from "../../widgets/ButtonList/ButtonList.stories"
 
 import { Feature } from "./Feature"
 
@@ -9,10 +10,19 @@ export default {
 
 const Template: ComponentStory<typeof Feature> = (args) => <Feature {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
-  todo: "KENTICO, UMBRACO, HEADLESS",
-  todo2: "CMS Integration",
-  todo3:
-    "I have experience with Kentico (Including Kentico Xperience 13 and Xperience by Kentico) and Umbraco CMS platforms. I have also built sites using headless CMS platforms, such as Kontent.ai and Sanity.io.",
+export const CMSIntegration = Template.bind({})
+CMSIntegration.args = {
+  id: "CMSIntegration",
+  kicker: "KENTICO, UMBRACO, HEADLESS",
+  heading: "CMS Integration",
+  text: "I have experience with Kentico (Including Kentico Xperience 13 and Xperience by Kentico) and Umbraco CMS platforms. I have also built sites using headless CMS platforms, such as Kontent.ai and Sanity.io.",
+}
+
+export const WithButtons = Template.bind({})
+WithButtons.args = {
+  id: "nextgen",
+  kicker: "REACT, VUE, CSS-IN-JS, JAMSTACK",
+  heading: "Building Next-Gen Websites",
+  text: "I can integrate my fundamental knowledge of web development (such as with vanilla Javascript and CSS ) with modern technology stacks, to create modular and maintainable websites.",
+  links: PrimarySecondary.args.buttons,
 }
